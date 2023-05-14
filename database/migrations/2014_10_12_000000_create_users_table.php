3d3660ef17ece0a4c2381247d4508925329c5485
pmhr18 <tmh6.pm18@gmail.com>
2023-05-14 22:20:05 +0900

    api_4 usersテーブルのマイグレーションファイルをOAuth機能に対応できるように修正

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 50);
+            $table->text('icon');
+            $table->string('profile', 200);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();;
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at')->nullable();
         });
     }
 
